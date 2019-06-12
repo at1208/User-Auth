20279313be7270e53588adaea8a67fd265805f2e
aman tiwari <amantiwari12081999@gmail.com>
2019-06-13 05:04:28 +0530

    dband route  setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+// require('./routes/user')(app)
+
+mongoose.connect('mongodb://localhost:27017/authentication')
+.then(() => console.log('Connected to Mongodb'))
+.catch((err) => console.log(err))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening to ${port}`))
